db7c53feeadf49183ff7b19e6e62bc445c3f16c5
Malik <inghammalik@icloud.com>
2023-02-12 03:32:53 -0500

    Finished project, Add more unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::process;
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let _config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", _config.query);
+    println!("In file {}", _config.file_path);
+
+    if let Err(e) = minigrep::run(_config) {
+        // pass
+    }
+
 }
